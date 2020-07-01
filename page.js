817d7220b347eff7efb6f0b8c28eec18b2ca731d
Praveen kumar <45269804+puttapraveen@users.noreply.github.com>
2020-07-01 13:25:59 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,11 +1,20 @@
 function show(){
-
 const value=document.getElementById("text").value;
+if(value==""){
+    alert("Please enter valid Goal!");
+}
+else{
 const a=document.querySelector("ul");
 const nw=document.createElement("li");
+var inp=document.createElement("input");
+inp.setAttribute('type','checkbox');
+inp.setAttribute('name','del');
+inp.setAttribute('id',value);
+nw.append(inp);
+nw.id=value;
 const text=document.createTextNode(value);
 nw.appendChild(text);
 a.appendChild(nw);
 console.log(a);
 document.getElementById("text").value="";
-}
+}}
